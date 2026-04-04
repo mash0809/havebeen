0fbda3f9ccac519fb23c213a93eb6094725fc921
mash0809 <tonem0809@gmail.com>
2026-04-04 19:17:04 +0900

    feat: 시뮬레이션 도메인 모델 정의

---
+++ src/main/kotlin/com/could/havebeen/stock/model/StockPrice.kt
@@ -0,0 +1,9 @@
+package com.could.havebeen.stock.model
+
+import java.math.BigDecimal
+import java.time.LocalDate
+
+data class StockPrice(
+    val date: LocalDate,
+    val closePrice: BigDecimal
+)
